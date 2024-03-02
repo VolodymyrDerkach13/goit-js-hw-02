f8bbc721c443c0e597ab19fdeeac4f182af357c7
VolodymyrDerkach13 <152654322+VolodymyrDerkach13@users.noreply.github.com>
2024-03-03 00:55:34 +0200

    hw2

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+function checkForSpam(message, spam, sale) {
+    const normalizedMessage = message.toLowerCase();
+    const hasSpamWord1 = normalizedMessage.includes("spam"); 
+    const hasSpamWord2 = normalizedMessage.includes("sale");
+
+  if (hasSpamWord1 || hasSpamWord2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
